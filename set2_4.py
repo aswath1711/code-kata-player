69932493836db333b144743a1fcc6197c929773f
aswath1711 <40084282+aswath1711@users.noreply.github.com>
2019-06-03 09:13:37 +0530

    Update set2_4.py

--- set2_4.py
+++ set2_4.py
@@ -4,4 +4,4 @@ stri=list(s)
 for ch in stri:
     if(ch=='A' or ch=='a' or ch=='E' or ch =='e' or ch=='I' or ch=='i' or ch=='O' or ch=='o' or ch=='U' or ch=='u'):
         stri.remove(ch)      
-print("".join(stri))
+print("".join(stri[::-1))
