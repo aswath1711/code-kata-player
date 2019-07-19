33500ae5cda7605ff1ed7c1cad085f82a585f821
aswath1711 <40084282+aswath1711@users.noreply.github.com>
2019-07-19 20:52:09 +0530

    Update set4_1.py

--- set4_1.py
+++ set4_1.py
@@ -5,16 +5,16 @@ Created on Fri Jul 19 20:40:56 2019
 @author: New
 """
 
-str=raw_input()
-li=list(str)
-count1=0
-count2=0
-for i in li:
-    if i=='(':
-        count1+=1
-    if i==')':
-        count2+=1
-if count1==count2:
-    print("yes")
+a=input()
+l=[]
+s=[]
+for i in range(0,len(a)):
+  if(a[i]=='('):
+    l.append(a[i])
+  elif(a[i]==')'):
+    s.append(a[i])
+if(len(l)==len(s)):
+  print("yes")
 else:
-    print("no")
+  print("no")
+© 2019 GitHub, Inc.
