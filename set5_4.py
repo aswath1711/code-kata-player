52bdcc455947c2b4754c6b3520b3339fb453350b
aswath1711 <40084282+aswath1711@users.noreply.github.com>
2019-07-19 23:21:27 +0530

    Update set5_4.py

--- set5_4.py
+++ set5_4.py
@@ -5,6 +5,8 @@ Created on Fri Jul 19 23:07:23 2019
 @author: New
 """
 import collections
+s=None
 k,n=map(str,input().split())
-de=collections.dequeue(k)
-print(de.rotate(int(n)))
+de=collections.deque(list(k))
+s=de.rotate(int(n))
+print(s)
