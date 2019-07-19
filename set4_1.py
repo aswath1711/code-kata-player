cc0e75d6d5b2bde79775fe298d63953be3a7d5f2
aswath1711 <40084282+aswath1711@users.noreply.github.com>
2019-07-19 20:54:37 +0530

    Update set4_1.py

--- set4_1.py
+++ set4_1.py
@@ -6,15 +6,14 @@ Created on Fri Jul 19 20:40:56 2019
 """
 
 a=input()
-l=[]
-s=[]
+o=[]
+c=[]
 for i in range(0,len(a)):
   if(a[i]=='('):
-    l.append(a[i])
+    o.append(a[i])
   elif(a[i]==')'):
-    s.append(a[i])
+    c.append(a[i])
 if(len(l)==len(s)):
   print("yes")
 else:
   print("no")
-© 2019 GitHub, Inc.
