3856a427513b31d08d9b75f4ce5cb95e645ac673
aswath1711 <40084282+aswath1711@users.noreply.github.com>
2019-07-19 22:05:45 +0530

    Add files via upload

---
+++ set4_9.py
@@ -0,0 +1,15 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Fri Jul 19 21:44:48 2019
+
+@author: New
+"""
+n=int(input())
+f=0
+for i in range(0,number):
+  if(2**i==n):
+    f=1
+if(f==1):
+   print("yes")
+else:
+  print("no")
