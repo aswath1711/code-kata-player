7ccacdd62165430694569b0ed91582f8f52f3c96
aswath1711 <40084282+aswath1711@users.noreply.github.com>
2019-07-19 23:47:40 +0530

    Add files via upload

---
+++ set5_7.py
@@ -0,0 +1,12 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Fri Jul 19 23:46:18 2019
+
+@author: New
+"""
+
+a=list(map(int,input().split(" ")))
+if(a[0]+a[1]+a[2]==180):
+    print("yes")
+else:
+    print("no")
