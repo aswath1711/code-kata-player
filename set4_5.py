6071345dfb47908fa123a12309b413011c4dfd3c
aswath1711 <40084282+aswath1711@users.noreply.github.com>
2019-07-19 21:18:55 +0530

    Update set4_5.py

--- set4_5.py
+++ set4_5.py
@@ -5,17 +5,10 @@ Created on Fri Jul 19 21:13:16 2019
 @author: New
 """
 
-a=raw_input().split()
-y=[]
-for i in range(0,len(a)):
-	g=a[i]
-	r=len(g)
-	for j in range(0,r):
-		u=g.count(g[j])
-		y.append(u)
-	w=max(y)
-	for k in range(0,r):
-		if w==y[k]:
-			print g[k],
-			break
-	y=[]
+import string
+a=input()
+a.split()
+a=a.replace(" ","")
+b=[i for i in a if a.count(i)==1]
+c=' '.join(b)
+print(c.lower())
