68feeef2a652e90cfcf9d9bc400c31f5d1f09a7e
aswath1711 <40084282+aswath1711@users.noreply.github.com>
2019-07-19 23:29:53 +0530

    Update set5_4.py

--- set5_4.py
+++ set5_4.py
@@ -4,9 +4,9 @@ Created on Fri Jul 19 23:07:23 2019
 
 @author: New
 """
-import collections
-s=None
-k,n=map(str,input().split())
-de=collections.deque(list(k))
-s=de.rotate(int(n))
-print(s)
+num,k_shift=input().split()
+k_shift=int(k_shift)
+
+for i in range(k_shift):
+    num=num[-1]+num[:-1]
+print(num,end=' ')
