374cd94e3751ded0472735ebd58d2153438bcfc9
aswath1711 <40084282+aswath1711@users.noreply.github.com>
2019-07-19 20:21:22 +0530

    Add files via upload

---
+++ set3_10.py
@@ -0,0 +1,12 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Fri Jul 19 20:17:30 2019
+
+@author: New
+"""
+
+a,b=map(str,input().split())
+count=0
+for i in range(len(a)):
+    if a[i]!=b[i]:
+        count!=1
