3ba50289a9c351990fa3524266ee70110f1901b7
aswath1711 <40084282+aswath1711@users.noreply.github.com>
2019-07-21 16:21:14 +0530

    Add files via upload

---
+++ set6_9.py
@@ -0,0 +1,18 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Sun Jul 21 16:19:46 2019
+
+@author: New
+"""
+
+n1=int(input())
+s=str(input())
+a=""
+r=""
+for i in range(len(s)):
+    if s[i]=='1':
+        a=a+s[i]+" "
+    elif s[i]=='0':
+        r=r+a
+        a=""
+print(r.strip())
